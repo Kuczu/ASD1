32c8573fbccb4e78c9a3d860a45b75ddbc5ed09e
Kuczu <pawelkucz93@gmail.com>
2016-09-05 16:07:54 +0200

    add LinkedStack

---
+++ src/pl/kuczu/abstractDataType/linked/LinkedStack.java
@@ -0,0 +1,45 @@
+package pl.kuczu.abstractDataType.linked;
+
+public class LinkedStack {
+    private Link top; // use ONLY next from Link class
+
+    public LinkedStack() {
+        this.top = null;
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public void push(int value){
+        Link newNode = new Link(value);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public int pop(){
+        if(isEmpty()){
+            System.out.println("Stos jest pusty!");
+            return -1;
+        }
+
+        int returnValue = top.value;
+        top = top.next;
+
+        return returnValue;
+    }
+
+    public void displayStack(){
+        if(isEmpty()){
+            System.out.println("Stos jest pusty!");
+            return;
+        }
+
+        Link node = top;
+
+        while(node != null){
+            System.out.print(node.value + " ");
+            node = node.next;
+        }
+    }
+}
