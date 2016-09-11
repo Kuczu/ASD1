e3a40212bfbaf86737ab5a9e8af2c03c499d4c5d
Kuczu <pawelkucz93@gmail.com>
2016-09-11 16:46:23 +0200

    add TODO HeapMerge

--- src/pl/kuczu/heaps/HeapMerge.java
+++ src/pl/kuczu/heaps/HeapMerge.java
@@ -1,6 +1,25 @@
 package pl.kuczu.heaps;
 
-public class HeapMerge {
+public class HeapMerge{
+    private int sequenceArray[][]; // [amountOfSequences][sequence]
+    private int sequenceLength[];
+    private int sequenceNumber;
 
+    public HeapMerge(int[][] sequenceArray) {
+        this.sequenceArray = sequenceArray;
+        this.sequenceLength = new int[sequenceArray.length];
+        this.sequenceNumber = sequenceLength.length;
 
+        for(int i = 0; i < sequenceNumber; i++){
+            sequenceLength[i] = sequenceArray[i].length;
+        }
+    }
+
+    public int[] mergeSequences(){
+        MinHeap heap = new MinHeap(sequenceNumber);
+
+        //TODO
+
+        return new int[]{-1};
+    }
 }
