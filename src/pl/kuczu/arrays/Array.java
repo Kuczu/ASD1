c276988c419e176cc7e3ea2a73f3602d53ac53e3
Kuczu <pawelkucz93@gmail.com>
2016-09-12 16:08:24 +0200

    add comments & fix bugs

--- src/pl/kuczu/arrays/Array.java
+++ src/pl/kuczu/arrays/Array.java
@@ -2,62 +2,90 @@ package pl.kuczu.arrays;
 
 import java.util.Random;
 
+/**
+ * W klasie zaimplementowane zostały podstawowe operacje na tablicy
+ */
 public class Array {
-    protected int Size;
-    protected long Array[];
-    protected int CurrPosition;
+    protected int size;
+    protected long array[];
+    protected int currPosition;
 
+    /**
+     * Konstruktor tablicy, automatycznie wypełnia ją losowymi liczbami.
+     * @see Array#fillArrayRandomNumbers()
+     * @param size wielkość tablicy - alokowanego miejsca
+     */
     public Array(int size){
-        Size = size;
-        Array = new long[size];
+        this.size = size;
+        array = new long[size];
 
         fillArrayRandomNumbers();
     }
 
+    /**
+     * Wypełnia całą tablicę losowymi liczbami (nadpisuje istniejące)
+     */
     public void fillArrayRandomNumbers(){
         Random rand = new Random();
 
-        for(int i = 0; i < Size; i++){
-            Array[i] = rand.nextLong();
+        for(int i = 0; i < size; i++){
+            array[i] = rand.nextLong();
         }
 
-        CurrPosition = Size - 1;
+        currPosition = size - 1;
     }
 
+    /**
+     * Na daną pozycję zapisuje zapisuje podaną wartość, złożoność czasowa O(1)
+     * @param value wartośc do zapisania w tablicy
+     * @param position pozaycja może być taka do jakiej tablica jest aktualnie wypełniona lub pozycję o jeden dalej
+     */
     public void insert(long value, int position){
-        if(position >= Size && position > position + 1){
+        if(position >= size && currPosition + 1 < position){
             System.out.println("Pozycja poza zakresem!");
             return;
         }
 
-        Array[position] = value;
+        array[position] = value;
+
+        if(currPosition < position){
+            currPosition++;
+        }
     }
 
+    /**
+     * Funkcja usuwa z podanej pozycji liczbę, pesymistyczna złożoność czasowa O(n)
+     * @param position pozycja z jakiej liczba ma być usunięta
+     */
     public void delete(int position){
-        if(position >= Size && position > position + 1){
+        if(position >= size && currPosition < position){
             System.out.println("Pozycja poza zakresem!");
             return;
         }
 
         //Override value to 0
-        //Array[position] = 0;
+        //array[position] = 0;
 
         //Copy everything by 1 position from right to position
-        for(; position <= CurrPosition; position++){
-            Array[position] = Array[position + 1];
+        for(; position <= currPosition; position++){
+            array[position] = array[position + 1];
         }
 
-        CurrPosition--;
+        currPosition--;
     }
 
+    /**
+     * Funkcja sprawdza czy liczby w całej tablicy są ułożone w porządku niemalejącym, pesymistyczna złożoność czasowa O(n)
+     * @return true jeśli tablica jest niemalejąca
+     */
     public boolean isOrder(){
         int currPosition = 0;
 
-        long temp = Array[currPosition];
+        long temp = array[currPosition];
 
-        for(currPosition++; currPosition <= CurrPosition; currPosition++){
-            if(temp <= Array[currPosition]){
-                temp = Array[currPosition];
+        for(currPosition++; currPosition <= this.currPosition; currPosition++){
+            if(temp <= array[currPosition]){
+                temp = array[currPosition];
             }
             else{
                 return false;
@@ -67,14 +95,26 @@ public class Array {
         return true;
     }
 
+    /**
+     * Funkcja usuwa duplikaty z tablicy, złożoność czasowa O(n^2)
+     * Funkcja działa tak, że dla każdego elementu z tablicy (pierwsza pętla) sprawdza kolejne liczby aż do końca tablicy (druga pętla),
+     * po napodkaniu duplikatu przepisuje ostatni element (jeśli nie jest duplikatem) na miejsce duplikatu,
+     * natomiast jeśli ostatni element jest także duplikatem, tablica jest pomniejszana tak długo aż nie napodka na liczbę nie będącą duplikatem
+     */
     public void del_Dupl(){
-        for(int currPosition = 0; currPosition <= CurrPosition; currPosition++){
-            long temp = Array[currPosition];
+        for(int currPosition = 0; currPosition <= this.currPosition; currPosition++){
+            long temp = array[currPosition];
 
-            for(int current = currPosition + 1; current <= CurrPosition; current++){
-                if(temp == Array[current]){
-                    Array[current] = Array[CurrPosition];
-                    CurrPosition--;
+            for(int current = currPosition + 1; current <= this.currPosition; current++){
+                if(temp == array[current]){
+                    while(temp == array[this.currPosition] && this.currPosition > currPosition){
+                        this.currPosition--;
+                    }
+
+                    if(this.currPosition >= current){
+                        array[current] = array[this.currPosition];
+                        this.currPosition--;
+                    }
                 }
             }
         }
--- src/pl/kuczu/arrays/SimpleArraySorting.java
+++ src/pl/kuczu/arrays/SimpleArraySorting.java
@@ -1,36 +1,47 @@
 package pl.kuczu.arrays;
 
-import pl.kuczu.arrays.Array;
-
+/**
+ * W klasie zaimplementowane zostały trzy algorytmy sortujące z rodziny "prostych algorytmów sortujących",
+ * wszystkie mają złożoność czasową O(n^2) i pamięciową O(1), lecz najowlniejszym z nich jest {@link SimpleArraySorting#bubbleSort},
+ * wydajniejszy z nich jest {@link SimpleArraySorting#slectionSort}, najszybszy natomiast jest {@link SimpleArraySorting#insertionSort()} który może być nawet 2 razy szybszy od BubbleSort
+ */
 public class SimpleArraySorting extends Array {
     public SimpleArraySorting(int size){
         super(size);
     }
 
-    void swap(int a, int b){
-        long temp = Array[a];
-        Array[a] = Array[b];
-        Array[b] = temp;
+    private void swap(int a, int b){
+        long temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 
-    void bubbleSort(){
-        for(int k = Size - 1; k >= 1; k--){
+    /**
+     * Algorytm ma złożoność czasową O(n^2), jest stabilny
+     * Funkcja porównuje dwa sąsiadujące obok siebie elementy - druga pętla, po porównaniu na końcu tablicy znajduje się największy element - pierwsza pętla zmienjsza zakres o 1
+     */
+    public void bubbleSort(){
+        for(int k = size - 1; k >= 1; k--){
             for(int j = 0; j < k; j++){
-                if(Array[j] > Array[j + 1]){
+                if(array[j] > array[j + 1]){
                     swap(j, j + 1);
                 }
             }
         }
     }
 
-    void slectionSort(){
+    /**
+     * Algorytm ma złożoność czasową O(n^2), NIE jest stabilny
+     * Funkcja przeszukuje liniowo tablice w poszukiwaniu najmniejszego elementu, następnie element ten daje na początek zakresu który wyznacza pierwsza pętla
+     */
+    public void slectionSort(){
         int min;
 
-        for(int k = 0; k < Size - 1; k++){
+        for(int k = 0; k < size - 1; k++){
             min = k;
 
-            for(int j = k + 1; j < Size; j++){
-                if(Array[j] < Array[min]){
+            for(int j = k + 1; j < size; j++){
+                if(array[j] < array[min]){
                     min = j;
                 }
             }
@@ -39,20 +50,27 @@ public class SimpleArraySorting extends Array {
         }
     }
 
-    void insertionSort(){
+    /**
+     * Algorytm ma złożoność czasową O(n^2), jest stabilny,
+     * optymalny dla ciągów prawie posortowanych, dlatego może być stosowany jako końcowy etap bardziej złożonych algorytmów sortowania
+     * Pierwsza pętla funkcji przechodzi od początku tablicy do jej końca,
+     * natomiast w zagnieżdżonej pętli od końca zakresu wyznaczonego przez pierwszą pętlę, sprawdzany jest warunek czy kolejna (poprzednia) wartość jest większa od porównywanej,
+     * jeśli jest to wartość ta jest przesuwana (przepisywana) do komórki o kolejnym indeksie, po skończeniu pętli wartość porównywana jest przepisywana na wyznaczone miejsce
+     */
+    public void insertionSort(){
         int idx;
         long tempValue;
 
-        for(int k = 1; k < Size; k++){
+        for(int k = 1; k < size; k++){
             idx = k - 1;
-            tempValue = Array[k];
+            tempValue = array[k];
 
-            while(idx >= 0 && tempValue < Array[idx]){
-                Array[idx + 1] = Array[idx];
+            while(idx >= 0 && tempValue < array[idx]){
+                array[idx + 1] = array[idx];
                 idx--;
             }
 
-            Array[idx + 1] = tempValue;
+            array[idx + 1] = tempValue;
         }
     }
 }
